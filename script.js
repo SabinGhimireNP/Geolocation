cab4d5d84125da2ada1275e92478cf3298e8d942
Sabin Ghimire <sabinghimire223@gmail.com>
2025-07-08 12:41:29 +0545

    error handeling

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const speed = document.querySelector(".speed-value");
 const arrow = document.querySelector(".arrow");
 
-navigator.geolocation.watchPosition((data) => {
-  // console.log(data);
-  speed.textContent = Math.round(data.coords.speed);
-  arrow.style.transform = `rotate(${data.coords.heading}deg)`;
-});
+navigator.geolocation.watchPosition(
+  (data) => {
+    // console.log(data);
+    speed.textContent = Math.round(data.coords.speed);
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  },
+  (err) => alert("Error fetching location")
+);
