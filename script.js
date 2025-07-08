d27610e8ae9373c6cf6307561ca4e32dc4ed930d
Sabin Ghimire <sabinghimire223@gmail.com>
2025-07-08 12:41:08 +0545

    Compass working??

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ const arrow = document.querySelector(".arrow");
 navigator.geolocation.watchPosition((data) => {
   // console.log(data);
   speed.textContent = Math.round(data.coords.speed);
+  arrow.style.transform = `rotate(${data.coords.heading}deg)`;
 });
