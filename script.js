c0ba5637d83345654feee08d7140c0d7bda69fe3
Sabin Ghimire <sabinghimire223@gmail.com>
2025-07-08 12:00:02 +0545

    speedometer added

---
+++ script.js
@@ -0,0 +1,7 @@
+const speed = document.querySelector(".speed-value");
+const arrow = document.querySelector(".arrow");
+
+navigator.geolocation.watchPosition((data) => {
+  // console.log(data);
+  speed.textContent = Math.round(data.coords.speed);
+});
